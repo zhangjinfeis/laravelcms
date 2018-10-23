969730e105229d20bdc16fa5b481b507a9bdff9d
robotszhang <1580854909@qq.com>
2018-10-23 10:31:10 +0800

    zjf-10-23

--- resources/views/admin/article_cate/edit.blade.php
+++ resources/views/admin/article_cate/edit.blade.php
@@ -27,12 +27,12 @@
                 <input name="parent_id" type="hidden" value="">
                 <div class="form-group">
                     <label for="name"><span class="text-danger">* </span>分类名称</label>
-                    <input type="text" class="form-control" id="name" name="name" placeholder="菜单名称" style="width:400px;" value="{{$menu->name}}" />
+                    <input type="text" class="form-control" id="name" name="name_cn" placeholder="菜单名称" style="width:400px;" value="{{$menu->name_cn}}" />
                     <small class="form-text text-muted">1-20个字符</small>
                 </div>
                 <div class="form-group">
                     <label for="name_cn">英文名称</label>
-                    <input type="text" class="form-control" id="name_cn" name="name_cn" placeholder="英文名称" style="width:400px;" value="{{$menu->name_cn}}" />
+                    <input type="text" class="form-control" id="name_cn" name="name_en" placeholder="英文名称" style="width:400px;" value="{{$menu->name_en}}" />
                     <small class="form-text text-muted">1-20个字符</small>
                 </div>
                 <div class="form-group">
@@ -143,118 +143,65 @@
     <div id="as" style="display: none;">
         <form id="form1">
             <input type="hidden" name="cate_id" value="{{$menu->id}}" />
-                <div class="form-group">
-                    <label><span class="text-danger">* </span>字段名称</label>
-                    <input type="text" class="form-control" name="name" placeholder="字段名称">
-                    <small class="form-text text-muted">1-20个字符</small>
-                </div>
-                <div class="form-group">
-                    <label><span class="text-danger">* </span>组件类型</label>
-                    <select class="form-control" name="type" lay-filter="type">
-                        @foreach(config('config.config_type') as $k=>$v)
-                            <option value="{{$k}}">{{$v}}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="key"><span class="text-danger">* </span>键</label>
-                    <input type="text" class="form-control" id="key" name="key" placeholder="键" value="">
-                    <small class="form-text text-muted">取值的字段名</small>
-                </div>
-                <div class="form-group hide js-width">
-                    <label>宽度</label>
-                    <input type="text" class="form-control" name="width" placeholder="宽度" value="">
-                    <small class="form-text text-muted">图片的高度，0或空值表示不限制</small>
-                </div>
-                <div class="form-group hide js-height">
-                    <label>高度</label>
-                    <input type="text" class="form-control" name="height" placeholder="高度" value="">
-                    <small class="form-text text-muted">图片的高度，0或空值表示不限制</small>
-                </div>
-                <div class="form-group hide js-size">
-                    <label>图片允许大小</label>
-                    <input type="text" class="form-control" name="size" placeholder="图片允许大小" value="">
-                    <small class="form-text text-muted">单位：M，0或空值表示不限制</small>
-                </div>
-                <div class="form-group hide js-custom">
-                    <label>编辑器类型</label>
-                    <select class="form-control w400" name="custom">
-                        @foreach(config('config.ckeditor_custom') as $k=>$v)
-                            <option value="{{$k}}">{{$v}}</option>
-                        @endforeach
-                    </select>
-                    <small class="form-text text-muted"></small>
-                </div>
-                <div class="form-group">
-                    <label for="tips">小贴士</label>
-                    <input type="text" class="form-control" id="tips" name="tips" placeholder="小贴士" value="">
-                    <small class="form-text text-muted">字段的说明</small>
-                </div>
-                <div class="form-group">
-                    <label for="sort"><span class="text-danger">* </span>排序</label>
-                    <input type="text" class="form-control" id="sort" name="sort" placeholder="排序" value="500">
-                    <small class="form-text text-muted">默认500,数值越小排名越靠前</small>
-                </div>
-                <div class="h10"></div>
-                <div class="text-center">
-                    <button type="submit" class="btn btn-primary" onclick="return create_exattr();">新增</button>
-                </div>
+            <div class="form-group">
+                <label><span class="text-danger">* </span>字段名称</label>
+                <input type="text" class="form-control" name="name" placeholder="字段名称">
+                <small class="form-text text-muted">1-20个字符</small>
+            </div>
+            <div class="form-group">
+                <label><span class="text-danger">* </span>组件类型</label>
+                <select class="form-control" name="type" lay-filter="type">
+                    @foreach(config('config.config_type') as $k=>$v)
+                        <option value="{{$k}}">{{$v}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="key"><span class="text-danger">* </span>键</label>
+                <input type="text" class="form-control" id="key" name="key" placeholder="键" value="">
+                <small class="form-text text-muted">取值的字段名</small>
+            </div>
+            <div class="form-group hide js-width">
+                <label>宽度</label>
+                <input type="text" class="form-control" name="width" placeholder="宽度" value="">
+                <small class="form-text text-muted">图片的高度，0或空值表示不限制</small>
+            </div>
+            <div class="form-group hide js-height">
+                <label>高度</label>
+                <input type="text" class="form-control" name="height" placeholder="高度" value="">
+                <small class="form-text text-muted">图片的高度，0或空值表示不限制</small>
+            </div>
+            <div class="form-group hide js-size">
+                <label>图片允许大小</label>
+                <input type="text" class="form-control" name="size" placeholder="图片允许大小" value="">
+                <small class="form-text text-muted">单位：M，0或空值表示不限制</small>
+            </div>
+            <div class="form-group hide js-custom">
+                <label>编辑器类型</label>
+                <select class="form-control w400" name="custom">
+                    @foreach(config('config.ckeditor_custom') as $k=>$v)
+                        <option value="{{$k}}">{{$v}}</option>
+                    @endforeach
+                </select>
+                <small class="form-text text-muted"></small>
+            </div>
+            <div class="form-group">
+                <label for="tips">小贴士</label>
+                <input type="text" class="form-control" id="tips" name="tips" placeholder="小贴士" value="">
+                <small class="form-text text-muted">字段的说明</small>
+            </div>
+            <div class="form-group">
+                <label for="sort"><span class="text-danger">* </span>排序</label>
+                <input type="text" class="form-control" id="sort" name="sort" placeholder="排序" value="500">
+                <small class="form-text text-muted">默认500,数值越小排名越靠前</small>
+            </div>
+            <div class="h10"></div>
+            <div class="text-center">
+                <button type="submit" class="btn btn-primary" onclick="return create_exattr();">新增</button>
+            </div>
         </form>
     </div>
-    <script>
-        //弹出创建菜单窗口
-        function alert_win(){
-            $boot.win({id:'#as','title':'附加字段'});
-            return false;
-        }
-        //类型切换
-        $('select[name=type]').change(function(){
-            if($(this).val() == 3 || $(this).val() == 4){
-                //$('.js-value').hide().find('input[name=value]').val('');
-                $('.js-custom').hide();
-                $('.js-width').show();
-                $('.js-height').show();
-                $('.js-size').show();
-            }else if($(this).val() == 5){
-                //$('.js-value').hide().find('input[name=value]').val('');
-                $('.js-custom').show();
-                $('.js-width').show();
-                $('.js-height').show();
-                $('.js-size').hide();
-            }else{
-                //$('.js-value').show();
-                $('.js-custom').hide();
-                $('.js-width').hide().find('input[name=width]').val('');
-                $('.js-height').hide().find('input[name=height]').val('');
-                $('.js-size').hide().find('input[name=size]').val('');
-            }
-        });
-        //新增菜单提交
-        function create_exattr(){
-            var data = $('#form1').serialize();
-            var url = "/admin/article_cate/edit?id={{$menu->id}}&on=1";
-            $.ajax({
-                type:'post',
-                url:'/admin/article_exattr/create',
-                data:data,
-                success:function(res){
-                    if(res.status == 0){
-                        $boot.error({text:res.msg},function(){
-                            $('[name='+res.field+']').focus();
-                        });
-                    }else{
-                        $boot.success({text:res.msg},function(){
-                            window.location = url;
-                        });
-                    }
-                }
-            });
-            return false;
-        }
-
-    </script>
-
-
+    
     <script>
         //提交编辑
         function post_edit(){
