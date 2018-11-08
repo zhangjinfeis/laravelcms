6523a6fd6c9d967d5bd393f1fcb402d889643af8
robotszhang <1580854909@qq.com>
2018-11-08 20:13:08 +0800

    zjf-11-8

--- public/resources/plugs/ckeditor/custom/ckeditor_full.js
+++ public/resources/plugs/ckeditor/custom/ckeditor_full.js
@@ -7,21 +7,21 @@ CKEDITOR.editorConfig = function( config ) {
     config.removePlugins = 'elementspath,resize';
     config.image_previewText='图片预览区...'; //预览区域显示内容
     config.toolbarGroups = [
-        { name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+        { name: 'forms', groups: [ 'forms' ] },
         { name: 'styles', groups: [ 'styles' ] },
+        { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+        { name: 'paragraph', groups: [ 'align', 'list', 'indent', 'blocks', 'bidi', 'paragraph' ] },
+        { name: 'colors', groups: [ 'colors' ] },
+        { name: 'insert', groups: [ 'insert' ] },
         { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
         { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
-        { name: 'forms', groups: [ 'forms' ] },
-        { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-        { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
         { name: 'links', groups: [ 'links' ] },
-        { name: 'insert', groups: [ 'insert' ] },
-        { name: 'colors', groups: [ 'colors' ] },
         { name: 'tools', groups: [ 'tools' ] },
+        { name: 'document', groups: [ 'document', 'doctools', 'mode' ] },
         { name: 'others', groups: [ 'others' ] },
         { name: 'about', groups: [ 'about' ] }
     ];
 
-    config.removeButtons = 'Save,NewPage,Preview,Print,Templates,Cut,Copy,Paste,PasteText,PasteFromWord,SelectAll,Scayt,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Outdent,Indent,CreateDiv,Language,Iframe,ShowBlocks,About,RemoveFormat,CopyFormatting,BidiLtr,BidiRtl';
+    config.removeButtons = 'Save,NewPage,Preview,Print,Templates,Cut,Copy,Paste,PasteText,PasteFromWord,SelectAll,Scayt,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Subscript,Superscript,CopyFormatting,RemoveFormat,Outdent,Indent,CreateDiv,BidiLtr,BidiRtl,Language,Anchor,SpecialChar,Iframe,ShowBlocks,About,Smiley,Font,Styles,HorizontalRule';
 };
 
--- resources/views/home/index/index.blade.php
+++ resources/views/home/index/index.blade.php
@@ -1,6 +1,19 @@
 @extends("home.include.mother")
 
 @section("content")
+    <script src="https://cdn.ckeditor.com/ckeditor5/11.1.1/classic/ckeditor.js"></script>
 
-    welcome!
+    <textarea name="content" id="editor">
+        &lt;p&gt;This is some sample content.&lt;/p&gt;
+    </textarea>
+    <script>
+        ClassicEditor
+            .create( document.querySelector( '#editor' ) , {
+
+        })
+            .catch( error => {
+                console.error( error );
+            } );
+        ClassicEditor.builtinPlugins.map( plugin => plugin.pluginName );
+    </script>
 @endsection
